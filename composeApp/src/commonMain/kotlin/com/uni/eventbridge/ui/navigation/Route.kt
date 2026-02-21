3f6a0a70684d1e166cc0aacb88d852cd6ea1ba46
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2026-02-21 09:47:54 +0300

    feat: implement navigation routes and string mapping

---
+++ composeApp/src/commonMain/kotlin/com/uni/eventbridge/ui/navigation/Route.kt
@@ -0,0 +1,21 @@
+package com.uni.eventbridge.ui.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed interface Route {
+
+    @Serializable
+    data object Home : Route
+    @Serializable
+    data object Explore : Route
+    @Serializable
+    data object Account : Route
+    @Serializable
+    data class EventDetails(val eventId: String) : Route
+}
+fun Route.toNavString(): String = when (this) {
+    Route.Home -> "home"
+    Route.Explore -> "explore"
+    Route.Account -> "account"
+    is Route.EventDetails -> "event_details/$eventId"
+}
