b71a0a3f6630ebe29b6de8fbc397917808690bb4
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2026-02-21 09:58:43 +0300

    feat: add navigation-compose dependency and organize compose bundles

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -34,12 +34,9 @@ kotlin {
             implementation(libs.androidx.activity.compose)
         }
         commonMain.dependencies {
-            implementation(libs.compose.runtime)
-            implementation(libs.compose.foundation)
-            implementation(libs.compose.material3)
-            implementation(libs.compose.ui)
-            implementation(libs.compose.components.resources)
-            implementation(libs.compose.uiToolingPreview)
+            implementation(libs.bundles.compose.core)
+            implementation(libs.bundles.compose.navigation)
+
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
         }
