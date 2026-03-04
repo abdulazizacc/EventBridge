efa289e42f502a8ba0e8cd0f6026e6db939f01fd
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2026-03-04 14:03:21 +0300

    feat: replace homeModule with ViewModelModule and add EventDetailsViewModel

---
+++ composeApp/src/commonMain/kotlin/com/uni/eventbridge/di/ViewModelModule.kt
@@ -0,0 +1,17 @@
+package com.uni.eventbridge.di
+
+import com.uni.eventbridge.presentation.eventDetails.EventDetailsViewModel
+import com.uni.eventbridge.presentation.home.HomeViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val ViewModelModule = module {
+    viewModelOf(::HomeViewModel)
+    viewModel { params ->
+        EventDetailsViewModel(
+            eventRepository = get(),
+            eventId = params.get<Long>(),
+        )
+    }
+}
