6c37d46f91cad902ef86d9d485b59dfdbd3728ad
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2026-03-16 21:18:47 +0300

    feat: implement KMP PlatformLocationProvider for Android and iOS

--- composeApp/src/iosMain/kotlin/com/uni/eventbridge/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/com/uni/eventbridge/Platform.ios.kt
@@ -1,5 +1,12 @@
 package com.uni.eventbridge
 
+import com.uni.eventbridge.domain.model.Location
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.useContents
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import platform.CoreLocation.CLLocationManager
+import platform.CoreLocation.kCLLocationAccuracyBest
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
@@ -7,3 +14,31 @@ class IOSPlatform: Platform {
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
+
+actual class PlatformLocationProvider {
+
+    private val locationManager = CLLocationManager()
+
+    @OptIn(ExperimentalForeignApi::class)
+    actual suspend fun getLocation(): Location {
+
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+
+        val loc = locationManager.location
+            ?: error("Location unavailable")
+
+        val coordinate = loc.coordinate
+
+        return coordinate.useContents {
+            Location(
+                latitude = latitude,
+                longitude = longitude
+            )
+        }
+    }
+
+    actual fun getLocationFlow(): Flow<Location> {
+        return emptyFlow()
+    }
+}
