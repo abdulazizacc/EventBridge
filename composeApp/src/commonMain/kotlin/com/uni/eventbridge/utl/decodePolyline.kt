c36dd9fabc126845786dbe75bb7a126d2099a6e6
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2026-03-16 20:37:51 +0300

    feat: add utility function to decode polylines into locations

---
+++ composeApp/src/commonMain/kotlin/com/uni/eventbridge/utl/decodePolyline.kt
@@ -0,0 +1,36 @@
+package com.uni.eventbridge.utl
+
+import com.uni.eventbridge.domain.model.Location
+
+fun decodePolyline(encoded: String): List<Location> {
+    val poly = mutableListOf<Location>()
+    var index = 0
+    var lat = 0
+    var lng = 0
+
+    while (index < encoded.length) {
+        var shift = 0
+        var result = 0
+        var b: Int
+        do {
+            b = encoded[index++].code - 63
+            result = result or (b and 0x1f shl shift)
+            shift += 5
+        } while (b >= 0x20)
+        val dLat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+        lat += dLat
+
+        shift = 0
+        result = 0
+        do {
+            b = encoded[index++].code - 63
+            result = result or (b and 0x1f shl shift)
+            shift += 5
+        } while (b >= 0x20)
+        val dLng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+        lng += dLng
+
+        poly.add(Location(lat / 1e5, lng / 1e5))
+    }
+    return poly
+}
