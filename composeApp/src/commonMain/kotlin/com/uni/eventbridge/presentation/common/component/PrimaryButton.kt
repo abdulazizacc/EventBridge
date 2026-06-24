270240af5280c46e0b48ebd5aa81dff9ba5bb8fb
abdulazizacc <abdulaziz.a.azzawi@gmail.com>
2026-06-24 05:04:03 +0300

    refactor: update `PrimaryButton` to support custom background colors and simplify `JoinBottomBar` logic
    - Add `backgroundColor` parameter to `PrimaryButton` and remove internal `enabled` state logic.
    - Update `JoinBottomBar` to use `LightGreen` when a user has joined and `Primary` otherwise.
    - Remove `isFull` check and the `enabled` parameter from `JoinBottomBar`.
    - Add `LightGreen` color constant to the theme.

--- composeApp/src/commonMain/kotlin/com/uni/eventbridge/presentation/common/component/PrimaryButton.kt
+++ composeApp/src/commonMain/kotlin/com/uni/eventbridge/presentation/common/component/PrimaryButton.kt
@@ -12,10 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.uni.eventbridge.presentation.common.component.theme.Primary
-import com.uni.eventbridge.presentation.common.component.theme.Secondary
 import com.uni.eventbridge.presentation.common.component.theme.White
 
 @Composable
@@ -24,23 +24,21 @@ fun PrimaryButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {},
     label: String = "Button",
-    enabled: Boolean = true,
-    verticalPadding: Int = 12
+    verticalPadding: Int = 12,
+    backgroundColor: Color = Primary,
 ) {
-    val backgroundColor = if (enabled) Primary else Secondary
-    val textColor = if (enabled) White else White.copy(alpha = 0.6f)
     
     Box(
         modifier = modifier
             .fillMaxWidth()
             .wrapContentHeight()
             .background(color = backgroundColor, shape = RoundedCornerShape(verticalPadding.dp))
-            .clickable(enabled = enabled, onClick = onClick),
+            .clickable( onClick = onClick),
         contentAlignment = Alignment.Center
     ) {
         Text(
             text = label,
-            color = textColor,
+            color = White,
             style = MaterialTheme.typography.labelLarge,
             modifier = Modifier.padding(vertical = 12.dp)
         )
--- composeApp/src/commonMain/kotlin/com/uni/eventbridge/presentation/common/component/theme/Color.kt
+++ composeApp/src/commonMain/kotlin/com/uni/eventbridge/presentation/common/component/theme/Color.kt
@@ -9,5 +9,5 @@ val Secondary = Color(0xFFF1F5F9)
 val SlateGray = Color(0xFF475569)
 val Red = Color(0xFFEF4444)
 val SurfaceDark = Color(0xFF0F172A)
-
+val LightGreen = Color(0xFF4CAF50)
 
